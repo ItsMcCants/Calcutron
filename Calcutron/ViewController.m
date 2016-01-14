c2e6c5d44a8ee2b4436e55f66dd752f2d0ef0eca
Jamar Gibbs <jmg220p@gmail.com>
2016-01-14 12:31:22 -0800

    first push

--- Calcutron/ViewController.m
+++ Calcutron/ViewController.m
@@ -29,9 +29,6 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
